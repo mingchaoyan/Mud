5c51cb1bb1747d4ab70c4f452646cf920201a141
mingchaoyan <mingchaoyan@gmail.com>
2015-12-13 22:21:41 +0800

    添加触屏滚动

--- Assets/GameMain.cs
+++ Assets/GameMain.cs
@@ -6,6 +6,11 @@ public class GameMain : MonoBehaviour
 	bool isChatting = true;
 	public Vector2 scrollPosition;
 
+	public float scrollVelocity = 0f;
+	public float timeTouchPhaseEnded = 0f;
+	public float inertiaDuration = 0.5f;
+	public Vector2 lastDeltaPos;
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -17,6 +22,34 @@ public class GameMain : MonoBehaviour
 	{
 		if (Time.timeScale > 0 && Input.GetKeyDown (KeyCode.Escape)) {
 			Time.timeScale = 0;
+		}
+		UpdateHandleTouch ();
+	}
+
+	void UpdateHandleTouch ()
+	{
+		if (Input.touchCount > 0) {
+			if (Input.GetTouch (0).phase == TouchPhase.Moved) {
+				scrollPosition.y += Input.GetTouch (0).deltaPosition.y;
+				lastDeltaPos = Input.GetTouch (0).deltaPosition;
+			} else if (Input.GetTouch (0).phase == TouchPhase.Ended) {
+				print ("End:" + lastDeltaPos.y + "|" + Input.GetTouch (0).deltaTime);
+				if (Mathf.Abs (lastDeltaPos.y) > 20.0f) {
+					scrollVelocity = (int)(lastDeltaPos.y * 0.5 / Input.GetTouch (0).deltaTime);
+					print (scrollVelocity);
+				}
+				timeTouchPhaseEnded = Time.time;
+			}
+		} else {
+			if (scrollVelocity != 0.0f) {
+				// slow down
+				float t = (Time.time - timeTouchPhaseEnded) / inertiaDuration;
+				float frameVelocity = Mathf.Lerp (scrollVelocity, 0, t);
+				scrollPosition.y += frameVelocity * Time.deltaTime;
+				
+				if (t >= inertiaDuration)
+					scrollVelocity = 0;
+			}
 		}
 	}
 
@@ -78,24 +111,3 @@ public class GameMain : MonoBehaviour
 
 	}
 }
-
-//using UnityEngine;
-//using System.Collections;
-//
-//public class GameMain: MonoBehaviour
-//{
-//	public Vector2 scrollPosition;
-//	public string longString = "This is a long-ish string";
-//	void OnGUI ()
-//	{
-//		scrollPosition = GUILayout.BeginScrollView (scrollPosition, GUILayout.Width (100), GUILayout.Height (100));
-//		GUILayout.Label (longString);
-//		if (GUILayout.Button ("Clear"))
-//			longString = "";
-//		
-//		GUILayout.EndScrollView ();
-//		if (GUILayout.Button ("Add More Text"))
-//			longString += "\nHere is another line";
-//		
-//	}
-//}
